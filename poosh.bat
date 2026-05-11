c64cd9322ac1337c9b3bbb3db81a761d7e41c55c
Jim <jmb15@williams.edu>
2026-05-11 17:41:27 +0800

    poosh.bat

--- poosh.bat
+++ poosh.bat
@@ -1,6 +1,6 @@
-cp -f -r ./* ../../james-bern.github.io/
-cd ..
-./poosh.bat "website/poosh.bat"
+cp -f -r ./* ../../../james-bern.github.io/
+cd ../..
+./poosh.bat "(inner) poosh.bat"
 cd ../james-bern.github.io/
 git add --all
 git commit -m "poosh.bat"
